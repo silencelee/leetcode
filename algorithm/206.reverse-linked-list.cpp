0b1bc2a2d5878defb792ebd5dd5aac1e08753926
silencelee <lij311@qq.com>
2020-01-28 17:49:55 +0800

    Create 206.reverse-linked-list.cpp

---
+++ algorithm/206.reverse-linked-list.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (59.11%)
+ * Likes:    3430
+ * Dislikes: 78
+ * Total Accepted:    803.3K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * 
+ * Follow up:
+ * 
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return head;
+        
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+
+        while (cur != nullptr) {
+            ListNode* tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+
+        return pre;     
+    }
+};
+// @lc code=end
+
