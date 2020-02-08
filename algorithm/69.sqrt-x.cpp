e5bfd2df831d9cf562be2dcf9e68ecf54a005ba0
silencelee <lij311@qq.com>
2020-02-08 14:31:00 +0800

    Create 69.sqrt-x.cpp

---
+++ algorithm/69.sqrt-x.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=69 lang=cpp
+ *
+ * [69] Sqrt(x)
+ *
+ * https://leetcode.com/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (32.73%)
+ * Likes:    1047
+ * Dislikes: 1679
+ * Total Accepted:    473.9K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '4'
+ *
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x, where x is guaranteed to be a
+ * non-negative integer.
+ * 
+ * Since the return type is an integer, the decimal digits are truncated and
+ * only the integer part of the result is returned.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 4
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since 
+ * the decimal part is truncated, 2 is returned.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0;
+        int r = x;
+
+        while (l < r) {
+            int mid = (l + r + 1LL) >> 1;
+            if (mid <= x / mid) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return l;
+    }
+};
+// @lc code=end
+
