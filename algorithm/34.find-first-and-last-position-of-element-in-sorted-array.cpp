d6023b22650275268a1a07e65a26b80608f8576a
silencelee <lij311@qq.com>
2020-02-09 17:53:05 +0800

    Create 34.find-first-and-last-position-of-element-in-sorted-array.cpp

---
+++ algorithm/34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=34 lang=cpp
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (34.99%)
+ * Likes:    2512
+ * Dislikes: 114
+ * Total Accepted:    413.3K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * 
+ */
+
+// @lc code=start
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.empty()) return {-1, -1};
+
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        if (nums[l] != target) return {-1, -1};
+
+        int start = l;
+        r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = (l + r + 1) >> 1;
+            if (nums[mid] <= target) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        int end = l;
+        return {start, end};        
+    }
+};
+// @lc code=end
+
