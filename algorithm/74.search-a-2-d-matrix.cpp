0437922c08253b4bb0c88a06e765f902d1a8e729
silencelee <lij311@qq.com>
2020-02-11 17:22:35 +0800

    Create 74.search-a-2-d-matrix.cpp

---
+++ algorithm/74.search-a-2-d-matrix.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=74 lang=cpp
+ *
+ * [74] Search a 2D Matrix
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix/description/
+ *
+ * algorithms
+ * Medium (35.81%)
+ * Likes:    1282
+ * Dislikes: 138
+ * Total Accepted:    282.7K
+ * Total Submissions: 789.3K
+ * Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,50]]\n3'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the
+ * previous row.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * Output: false
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int l = 0;
+        int r = m * n - 1;
+
+        while (l < r) {
+            int mid = (l + r + 1LL) >> 1;
+            if (matrix[mid / n][mid % n] <= target) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            } 
+        }
+        
+        return (matrix[l / n][l % n] == target);
+    }
+};
+// @lc code=end
+
