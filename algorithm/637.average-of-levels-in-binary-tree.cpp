d5ba16d0e183d8f54ac92fb281a5683aea92ff3b
silencelee <lij311@qq.com>
2020-09-12 14:25:59 +0800

    Create 637.average-of-levels-in-binary-tree.cpp

---
+++ algorithm/637.average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=637 lang=cpp
+ *
+ * [637] Average of Levels in Binary Tree
+ *
+ * https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+ *
+ * algorithms
+ * Easy (61.36%)
+ * Likes:    1402
+ * Dislikes: 168
+ * Total Accepted:    134.8K
+ * Total Submissions: 212.6K
+ * Testcase Example:  '[3,9,20,15,7]'
+ *
+ * Given a non-empty binary tree, return the average value of the nodes on each
+ * level in the form of an array.
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * Output: [3, 14.5, 11]
+ * Explanation:
+ * The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on
+ * level 2 is 11. Hence return [3, 14.5, 11].
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The range of node's value is in the range of 32-bit signed integer.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> res;
+        if (root == nullptr) return res;
+
+        queue<TreeNode*> q;
+        q.emplace(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+
+            double d = 0.f;
+            for (int i = 0; i < n; ++i) {
+                auto t = q.front();
+                q.pop();
+
+                d += t->val;
+                if (t->left != nullptr) q.emplace(t->left);
+                if (t->right != nullptr) q.emplace(t->right);
+            }
+
+            res.emplace_back(d / n);
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
