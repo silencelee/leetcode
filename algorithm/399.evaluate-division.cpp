ddabca337c7b3889d9bf2b06557806c66e031c88
silencelee <lij311@qq.com>
2021-01-06 11:08:45 +0800

    Create 399.evaluate-division.cpp

---
+++ algorithm/399.evaluate-division.cpp
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode id=399 lang=cpp
+ *
+ * [399] Evaluate Division
+ *
+ * https://leetcode.com/problems/evaluate-division/description/
+ *
+ * algorithms
+ * Medium (53.74%)
+ * Likes:    3084
+ * Dislikes: 244
+ * Total Accepted:    166.5K
+ * Total Submissions: 308.9K
+ * Testcase Example:  '[["a","b"],["b","c"]]\n[2.0,3.0]\n[["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]'
+ *
+ * You are given an array of variable pairs equations and an array of real
+ * numbers values, where equations[i] = [Ai, Bi] and values[i] represent the
+ * equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a
+ * single variable.
+ * 
+ * You are also given some queries, where queries[j] = [Cj, Dj] represents the
+ * j^th query where you must find the answer for Cj / Dj = ?.
+ * 
+ * Return the answers to all queries. If a single answer cannot be determined,
+ * return -1.0.
+ * 
+ * Note: The input is always valid. You may assume that evaluating the queries
+ * will not result in division by zero and that there is no contradiction.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries =
+ * [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+ * Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+ * Explanation: 
+ * Given: a / b = 2.0, b / c = 3.0
+ * queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+ * return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: equations = [["a","b"],["b","c"],["bc","cd"]], values =
+ * [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+ * Output: [3.75000,0.40000,5.00000,0.20000]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: equations = [["a","b"]], values = [0.5], queries =
+ * [["a","b"],["b","a"],["a","c"],["x","y"]]
+ * Output: [0.50000,2.00000,-1.00000,-1.00000]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= equations.length <= 20
+ * equations[i].length == 2
+ * 1 <= Ai.length, Bi.length <= 5
+ * values.length == equations.length
+ * 0.0 < values[i] <= 20.0
+ * 1 <= queries.length <= 20
+ * queries[i].length == 2
+ * 1 <= Cj.length, Dj.length <= 5
+ * Ai, Bi, Cj, Dj consist of lower case English letters and digits.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_set<string> vertex;
+        unordered_map<string, unordered_map<string, double>> g;
+
+        for (int i = 0; i < equations.size(); ++i) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            double c = values[i];
+
+            vertex.insert(a);
+            vertex.insert(b);
+            g[a][b] = c;
+            g[b][a] = 1.0f / c;
+        }
+
+        for (auto k : vertex) {
+            for (auto i : vertex) {
+                for (auto j : vertex) {
+                    if (g[i][k] && g[k][j]) g[i][j] = g[i][k] * g[k][j];
+                }
+            }
+        }
+
+        vector<double> res;
+        for (int i = 0; i < queries.size(); ++i) {
+            string c = queries[i][0];
+            string d = queries[i][1];
+
+            if (g[c][d] > 0) {
+                res.emplace_back(g[c][d]);
+            } else {
+                res.emplace_back(-1);
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
